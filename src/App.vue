bf4c7f686235569f9b2f715f3cf7434a0858e164
Nikita0x <klirmio21@gmail.com>
2026-07-14 19:03:34 +0300

    чистый template для разработки Chrome Extensions: Vue + Typescript + Pinia

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 style="color: red;">Hello World. Counter is: {{ count }}</h1>
+        <VueButton />
+    </div>
+</template>
+
+<script setup lang="ts">
+import VueButton from './components/VueButton.vue';
+
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
+const counterStore = useCounterStore()
+const { count } = storeToRefs(counterStore)
+
+</script>
---
+++ src/components/VueButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="counterStore.increment">Increment count from VueButton</button>
+</template>
+
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore()
+
+</script>
