286ee9fde28ce50d7cb5fe071dfdfce4bedd5c0c
Nikita0x <klirmio21@gmail.com>
2026-08-03 09:09:08 +0300

    moved red dot. hid the clock icon

--- src/components/StreamCard.vue
+++ src/components/StreamCard.vue
@@ -23,6 +23,7 @@
 			/>
 
 			<div class="thumb-overlay">
+				<span class="live"></span>
 				<span class="thumb-user">{{ props.stream.user_name }}</span>
 			</div>
 		</div>
@@ -34,13 +35,26 @@
 			</div>
 			<div class="meta-row">
 				<div style="display: flex; align-items: center; gap: 5px">
-					<span class="live"></span>
+					<!-- <span class="live"></span> -->
+					<svg
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						aria-hidden="true"
+						style="fill: var(--color-red)"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M6 8a6 6 0 1 1 7.025 5.913l.012.036A3 3 0 0 0 15.883 16H17a4 4 0 0 1 4 4v2h-2v-2a2 2 0 0 0-2-2h-1.117A5 5 0 0 1 12 16.15 5 5 0 0 1 8.117 18H7a2 2 0 0 0-2 2v2H3v-2a4 4 0 0 1 4-4h1.117a3 3 0 0 0 2.846-2.051l.012-.036A6.002 6.002 0 0 1 6 8Zm6 4a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z"
+							clip-rule="evenodd"
+						></path>
+					</svg>
 					<AnimatedViewCount :count="props.stream.viewer_count" />
 				</div>
 
-				<span class="uptime"
-					><span style="transform: translateY(1px); display: inline-block">🕒</span
-					>{{ formatUptime(props.stream.started_at) }}</span
+				<span class="uptime">
+					<!-- <span style="transform: translateY(1px); display: inline-block">🕒</span> -->
+					{{ formatUptime(props.stream.started_at) }}</span
 				>
 			</div>
 		</div>
@@ -132,6 +146,11 @@ watch(
 	position: absolute;
 	left: 0;
 	bottom: 0;
+
+	display: flex;
+	align-items: center;
+	gap: 5px;
+
 	padding: 7px 9px;
 
 	background: rgba(0, 0, 0, 0.6);
